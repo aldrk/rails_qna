dc81c97ea50fb14459312b29d75b66c8235bf083
Alexandr <sashka.krup@gmail.com>
2021-09-15 00:20:36 +0300

    finish task

---
+++ db/migrate/20210914211549_create_answers.rb
@@ -0,0 +1,14 @@
+class CreateAnswers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :answers do |t|
+      t.string :title, null: false
+      t.text :body, null: false
+      t.references :question, null: false, foreign_key: true
+
+      t.timestamps
+    end
+
+    change_column_null :answers, :title, false
+    change_column_null :answers, :body, false
+  end
+end
