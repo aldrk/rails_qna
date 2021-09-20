93e17f025bf70dbb56f84f25e4b651fd7937673a
Alexandr <sashka.krup@gmail.com>
2021-09-21 00:11:55 +0300

    fix after review

--- spec/features/question/delete_spec.rb
+++ spec/features/question/delete_spec.rb
@@ -12,8 +12,12 @@ feature 'User can remove the question', "
 
   scenario 'author tries to destroy question' do
     sign_in(user)
-
+    expect(page).to have_content 'MyQuestionTitle'
+    expect(page).to have_content 'MyQuestionBody'
     click_on 'Delete'
+
+    expect(page).not_to have_content 'MyQuestionTitle'
+    expect(page).not_to have_content 'MyQuestionBody'
 
     expect(page).to have_content 'Question was destroyed'
   end
